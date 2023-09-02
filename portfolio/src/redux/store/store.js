9a2574b337a07e196dff0707d6c1fa532d196c31
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-09-02 23:02:36 +0200

    created structure and downloaded libraries

---
+++ portfolio/src/redux/store/store.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+    //cart: cartReducer,
+    //settings: settingsReducer,
+    // Aggiungi altri reducer qui come i commenti sopra
+});
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false, // Disabilita il controllo serializzabile per gli slice che utilizzano funzioni non serializzabili come gli action creator thunk
+    }).concat(thunk),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
